95a0ad02bfba9785f4a7b7bb4c05067cddc13420
lenovo <2069570851@qq.com>
2017-03-02 13:33:17 +0800

    android javascript交互 打电话运行权限

--- app/src/main/java/com/example/lenovo/demojs/MainActivity.java
+++ app/src/main/java/com/example/lenovo/demojs/MainActivity.java
@@ -4,7 +4,9 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
     private Button mBtn1, mBtn2;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONR = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,10 +76,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @JavascriptInterface
-    public void call(final String phone) {
-        Log.v("law", "号码是" + phone);
-        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
+    public void call(View view) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONR);
+        } else {
+            callPhone();
+        }
+    }
 
+    private void callPhone() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri date = Uri.parse("tel:" + "10086");
+        intent.setData(date);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -88,6 +99,19 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==MY_PERMISSIONS_REQUEST_CALL_PHONR){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                callPhone();
+            }else {
+                Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_LONG).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @JavascriptInterface
